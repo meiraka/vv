4ea3a33da29002f5cc071121f8f994d5b08be74c
meiraka <meiraka.pre@gmail.com>
2020-09-21 13:04:49 +0900

    test default config value

--- config_test.go
+++ config_test.go
@@ -66,6 +66,57 @@ func TestParseConfig(t *testing.T) {
 	}
 }
 
+func TestParseConfigDefault(t *testing.T) {
+	config, date, err := ParseConfig(nil, "example.config.yaml")
+	if err != nil {
+		t.Fatalf("failed to parse: %v", err)
+	}
+	if !date.IsZero() {
+		t.Errorf("got date %v; want zer", date)
+	}
+	want := &Config{}
+	want.MPD.Network = "tcp"
+	want.MPD.Addr = "localhost:6600"
+	want.MPD.Conf = "/etc/mpd.conf"
+	want.Server.Addr = ":8080"
+	want.Server.CacheDirectory = "/tmp/vv"
+	want.Server.Cover.Local = true
+	want.Playlist.Tree = map[string]*ConfigListNode{
+		"AlbumArtist": {
+			Sort: []string{"AlbumArtist", "Date", "Album", "DiscNumber", "TrackNumber", "Title", "file"},
+			Tree: [][2]string{{"AlbumArtist", "plain"}, {"Album", "album"}, {"Title", "song"}},
+		},
+		"Album": {
+			Sort: []string{"AlbumArtist-Date-Album", "DiscNumber", "TrackNumber", "Title", "file"},
+			Tree: [][2]string{{"AlbumArtist-Date-Album", "album"}, {"Title", "song"}},
+		},
+		"Artist": {
+			Sort: []string{"Artist", "Date", "Album", "DiscNumber", "TrackNumber", "Title", "file"},
+			Tree: [][2]string{{"Artist", "plain"}, {"Title", "song"}},
+		},
+		"Genre": {
+			Sort: []string{"Genre", "Album", "DiscNumber", "TrackNumber", "Title", "file"},
+			Tree: [][2]string{{"Genre", "plain"}, {"Album", "album"}, {"Title", "song"}},
+		},
+		"Date": {
+			Sort: []string{"Date", "Album", "DiscNumber", "TrackNumber", "Title", "file"},
+			Tree: [][2]string{{"Date", "plain"}, {"Album", "album"}, {"Title", "song"}},
+		},
+		"Composer": {
+			Sort: []string{"Composer", "Date", "Album", "DiscNumber", "TrackNumber", "Title", "file"},
+			Tree: [][2]string{{"Composer", "plain"}, {"Album", "album"}, {"Title", "song"}},
+		},
+		"Performer": {
+			Sort: []string{"Performer", "Date", "Album", "DiscNumber", "TrackNumber", "Title", "file"},
+			Tree: [][2]string{{"Performer", "plain"}, {"Album", "album"}, {"Title", "song"}},
+		},
+	}
+	want.Playlist.TreeOrder = []string{"AlbumArtist", "Album", "Artist", "Genre", "Date", "Composer", "Performer"}
+	if !reflect.DeepEqual(config, want) {
+		t.Errorf("got %+v; want %+v", config, want)
+	}
+}
+
 func TestConfigYAML(t *testing.T) {
 	f, err := os.Open("./appendix/example.config.yaml")
 	if err != nil {
