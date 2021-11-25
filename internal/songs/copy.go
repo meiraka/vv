e57ee968f6b87ac8b6ef865fd71143281567a4bb
meiraka <meiraka.pre@gmail.com>
2021-11-25 20:00:12 +0900

    use copy

--- internal/songs/copy.go
+++ internal/songs/copy.go
@@ -3,8 +3,6 @@ package songs
 // Copy shallow copies songs.
 func Copy(s []map[string][]string) []map[string][]string {
 	n := make([]map[string][]string, len(s))
-	for i := range s {
-		n[i] = s[i]
-	}
+	copy(n, s)
 	return n
 }
